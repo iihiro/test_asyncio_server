199d2def975c46df01c9387c301b265923c92573
Hiromichi IIZUKA <iizuka@eduam.co.jp>
2019-09-13 00:32:43 +0900

    Add some sample

--- c.py
+++ c.py
@@ -2,11 +2,11 @@
 
 import asyncio
 
-async def test_client(host, port, n):
+async def test_client(host, port, n, interval_sec):
     
     reader, writer = await asyncio.open_connection(host, port)
 
-    for i in range(n):
+    async def func(i):
         smsg = 'hello %d\n' % i
         print('[c] send %s' % smsg)
         writer.write(smsg.encode())
@@ -14,13 +14,23 @@ async def test_client(host, port, n):
         rmsg = await reader.readline()
         print('[c] recv %s' % rmsg.decode())
 
+    if n < 0:
+        i = 0
+        while True:
+            await func(i)
+            i += 1
+            await asyncio.sleep(interval_sec)
+    else:
+        for i in range(n):
+            await func(i)
+            await asyncio.sleep(interval_sec)
+
     writer.close()
     
 
-def start(host, port):
-    n = 10
+def start(host, port, n=-1, interval_sec=0):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(test_client(host, port, n))
+    loop.run_until_complete(test_client(host, port, n, interval_sec))
 
 if __name__ == '__main__':
     start('127.0.0.1', 12345)
--- sc.py
+++ sc.py
@@ -4,6 +4,36 @@ import time
 import multiprocessing
 import c
 
+def test3_3(host='127.0.0.1', port=12345):
+    import s3
+    disp = s3.Dispatcher()
+    disp.start_daemon(host, port)
+
+    async def putdata():
+        for i in range(5):
+            await asyncio.sleep(1)
+            disp.put('putdata#%d' % i)
+    
+    import asyncio
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(asyncio.gather(
+        putdata(),
+        c.test_client(host, port, 10, 0.5)))
+    
+
+def test3_2(host='127.0.0.1', port=12345):
+    import s3
+    disp = s3.Dispatcher()
+    disp.start_daemon(host, port)
+
+    c.start(host, port, n=10, interval_sec=0.5)
+
+def test3_1(host='127.0.0.1', port=12345):
+    import s3
+    s3.start_daemon(host, port)
+
+    c.start(host, port)
+
 def test2(host='127.0.0.1', port=12345):
     import s2
     s2.start_daemon(host, port)
@@ -21,4 +51,7 @@ def test1(host='127.0.0.1', port=12345):
 
 if __name__ == '__main__':
     #test1()
-    test2()
+    #test2()
+    #test3_1()
+    #test3_2()
+    test3_3()
